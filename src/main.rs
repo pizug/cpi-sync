0a0f5f4a8f919ed63dc8fae019b8eaf240b4a8c2
Fatih Pense <fatihpense@gmail.com>
2021-01-31 01:52:45 +0300

    deserialize config

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,53 @@
-use std::{error::Error, fs::File, io::BufReader};
+use std::{error::Error, fs::File, io::Read};
 
 use clap::Clap;
 use jsonschema::{self, Draft, JSONSchema};
+use serde::{Deserialize, Serialize};
 use serde_json::{self, Value};
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Package {
+    id: String,
+    local: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct CredentialSUser {
+    username: String,
+    password_environment_variable: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct CredentialOauthClientCredentials {
+    client_id: String,
+    token_endpoint_url: String,
+    client_secret_environment_variable: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum CredentialInside {
+    #[serde(rename = "oauth_client_credentials")]
+    OauthClientCredentials(CredentialOauthClientCredentials),
+    #[serde(rename = "s_user")]
+    SUser(CredentialSUser),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Tenant {
+    host: String,
+    credential: CredentialInside,
+    // credential: CredentialInside,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    cpisync: String,
+    tenant: Tenant,
+    packages: Vec<Package>,
+}
+
 #[derive(Clap, Debug)]
-#[clap(version = "0.1", author = "Fatih Pense @ pizug.com")]
+#[clap(version = "0.1.0", author = "Fatih Pense @ pizug.com")]
 struct Opts {
     #[clap(short, long, default_value = "cpi-sync.json")]
     config: String,
@@ -21,11 +63,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         .with_draft(Draft::Draft7)
         .compile(&json_schema)?;
 
-    let file = File::open(&opts.config)?;
-    let reader = BufReader::new(file);
+    let mut config_str = String::new();
+    File::open(&opts.config)?.read_to_string(&mut config_str)?;
+    // let reader = BufReader::new(file);
 
     // Read the JSON contents of the file as an instance of `User`.
-    let config_json: serde_json::Value = serde_json::from_reader(reader)?;
+    let config_json: serde_json::Value = serde_json::from_str(&config_str)?;
 
     let result = compiled_schema.validate(&config_json);
     if let Err(errors) = result {
@@ -34,6 +77,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
+    let config: Config = serde_json::from_str(&config_str)?;
+
+    println!("config: {:?}", config);
     //println!("Using input file: {:?}", opts);
 
     Ok(())
